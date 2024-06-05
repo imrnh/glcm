c43d355ba66d54c2fd86ac64aaab49c3d6dc4b5f
Muhtasir Imran <mu.imran2002@gmail.com>
2024-06-06 01:09:05 +0600

    Added ceil

--- main.py
+++ main.py
@@ -6,29 +6,30 @@ from utils import get_entropy, get_contrast, get_homogeneity, inverse_diff_momen
 if __name__ == "__main__":
     os.makedirs("sims", exist_ok=True)
 
-    image = [
-        [0, 3, 0, 8, 1, 6, 6, 2, 1, 1, 0, 2, 4],
-        [0, 9, 2, 2, 5, 3, 2, 4, 5, 0, 2, 5, 1],
-        [3, 6, 1, 0, 0, 5, 7, 0, 0, 6, 1, 0, 0],
-        [5, 3, 7, 3, 0, 2, 5, 3, 2, 0, 7, 3, 6],
-        [5, 5, 5, 0, 1, 3, 7, 4, 1, 5, 5, 5, 0],
-        [5, 4, 7, 0, 5, 4, 2, 6, 5, 6, 7, 7, 5],
-        [6, 3, 0, 2, 8, 6, 4, 3, 0, 5, 0, 3, 6],
-        [7, 5, 1, 0, 4, 3, 3, 6, 4, 6, 1, 7, 5],
-        [4, 3, 4, 2, 9, 6, 4, 3, 1, 7, 4, 3, 6],
-        [1, 3, 3, 4, 2, 2, 2, 7, 4, 4, 3, 8, 7],
-        [3, 7, 1, 8, 1, 6, 0, 2, 1, 7, 1, 2, 4],
-        [7, 4, 1, 4, 3, 1, 2, 3, 5, 4, 1, 3, 7],
-        [0, 1, 2, 9, 3, 7, 2, 6, 3, 4, 2, 7, 4]
-    ]
+    image = np.array([
+    [0, 9, 2, 8, 1, 3, 4, 2, 1, 3, 2, 2, 4],
+    [7, 6, 1, 6, 2, 4, 6, 1, 4, 3, 1, 1.5, 3],
+    [7, 5, 3, 7, 5, 6, 1, 5, 7, 5, 3, 6.5, 3],
+    [2, 8, 6, 1, 6, 0, 2, 6, 6, 3, 6, 7.5, 5],
+    [3, 6, 1, 6, 2, 5, 3, 1, 4, 1, 1, 1.5, 3],
+    [5, 3, 7, 2, 4, 3, 7, 4, 4, 2, 7, 5.5, 1],
+    [8, 0, 6, 5, 0, 4, 0, 5, 2, 4, 6, 6, 2],
+    [1, 3, 3, 8, 0, 2, 0, 6, 0, 3, 3, 7, 4],
+    [7, 7, 1, 6, 3, 7, 5, 5, 5, 7, 1, 6.5, 3],
+    [2, 0, 6, 4, 3, 5, 7, 3, 5, 3, 6, 3.5, 7],
+    [8, 8, 6, 7, 4, 1, 6, 1, 6, 6, 6, 1.5, 3],
+    [7, 2, 3, 2, 8, 0, 6, 3, 0, 1, 3, 3, 6],
+    [0, 9, 2, 2, 4, 2, 3, 0, 4, 1, 2, 0.5, 1]
+])
 
-    image_rounded = np.rint(np.array(image)).astype(int)
-    displacement = [0, 2]
+    image_rounded = np.rint(np.ceil(np.array(image))).astype(int)
+    displacement = [0, -1]
     glcm = make_glcm(image_rounded, displacement, levels=9)
     normalized_glcm = glcm / sum(sum(glcm))
+    normalized_glcm = np.round(normalized_glcm, 2)
 
     print(f"GLCM: {glcm}")
-    print(f"Normalized GLCM: {normalized_glcm}")
+    print(f"Normalized GLCM:\n {normalized_glcm}")
 
     print(f"Entropy: {get_entropy(normalized_glcm)}")
     print(f"Contrast: {get_contrast(normalized_glcm)}")
